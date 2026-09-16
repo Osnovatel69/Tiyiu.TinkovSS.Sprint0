4c3d7fe7a9ade398487614c26a7c132f834e9c8b
TinkovSS <tinkovsergey05@mail.ru>
2026-09-16 19:55:08 +0500

    Task1

---
+++ Tyuiu.TinkovSS.Sprint0.Task10.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyuiu.TinkovSS.Sprint0.Task10.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Сергей";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+        }
+    }
+}
