822ee23b5a96e2be0d6247ca83cc22d949e2f37c
TinkovSS <tinkovsergey05@mail.ru>
2026-09-16 23:10:30 +0500

    Task6

---
+++ Tyuiu.TinkovSS.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.TinkovSS.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static object AdditionArray(int[] numbers)
+        {
+            var total = 0;
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }
+            return total;
+        }
+
+        public static object SubtractionArray(int[] numbers)
+        {
+            var total = numbers[0];
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                total = total - numbers[i];
+            }
+            return total;
+        }
+
+        public static object MultiplicationArray(int[] numbers)
+        {
+            var total = 1;
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                total = total * numbers[i];
+            }
+            return total;
+        }
+    }
+}
