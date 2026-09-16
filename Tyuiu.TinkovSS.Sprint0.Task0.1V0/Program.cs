326a6ea0c178e60e32822a53bdf24f4b08b7d78a
TinkovSS <tinkovsergey05@mail.ru>
2026-09-16 19:47:12 +0500

    Add project files.

---
+++ Tyuiu.TinkovSS.Sprint0.Task0.1V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TinkovSS.Sprint0.Task0._1V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
